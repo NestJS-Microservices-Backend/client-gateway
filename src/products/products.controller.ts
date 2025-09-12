54667e3428e9d1c4c02082a176cd0769668c971f
ltomassini <leandroctomassini@gmail.com>
2025-09-12 14:44:53 -0300

    feat: Integrate products microservice communication
    Introduces the necessary configuration and modules to connect the client gateway with the products microservice.
    
    Key changes include:
    
    - Added @nestjs/microservices dependency.
    
    - Configured ClientsModule in the ProductsModule to establish a TCP connection to the products microservice.
    
    - Updated environment variable handling (envs.ts) to include host and port for the products microservice.
    
    - Added a service token PRODUCT_SERVICE for dependency injection.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
 
 
 
@@ -6,7 +6,9 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 @Controller( 'products' )
 export class ProductsController {
 
-  constructor() { }
+  constructor(
+    @Inject()
+  ) { }
 
   @Post()
   createProduct() {
