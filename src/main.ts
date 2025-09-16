2d3887d49ffddcdbc27769897af5d5f4d2ad1725
ltomassini <leandroctomassini@gmail.com>
2025-09-16 16:15:06 -0300

    fix: update console log message for test

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   await app.listen( envs.port );
 
-  console.log( 'Test' );
+  console.log( 'Test ok' );
 
   logger.log( `Gateway running on port ${ envs.port }` );
 }
