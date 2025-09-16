45125c69349770551615d628b5f6c372ddbe2963
ltomassini <leandroctomassini@gmail.com>
2025-09-16 15:34:57 -0300

    docs: Enhance and complete project README

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -5,9 +5,17 @@ import { RpcException } from '@nestjs/microservices';
 export class RpcCustomExceptionFilter implements ExceptionFilter {
   catch( exception: RpcException, host: ArgumentsHost ) {
     const ctx = host.switchToHttp();
+
     const response = ctx.getResponse();
 
     const rpcError = exception.getError();
+
+    if ( rpcError.toString().includes( 'Empty response' ) ) {
+      return response.status( 500 ).json( {
+        status: 500,
+        message: rpcError.toString().substring( 0, rpcError.toString().indexOf( '(' ) - 1 )
+      } );
+    }
 
     if (
       typeof rpcError === 'object' &&
